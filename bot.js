85446abd099aaa15497965f5083bb39734095ee3
hamossss <40631448+hamossss@users.noreply.github.com>
2018-06-29 22:38:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -103,6 +103,53 @@ let welcomer = member.guild.channels.find("name","chat");
       }
       });
 
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "اسم سرفر")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "اسم سرفر")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+    
+    channel.send(`**KD نورت كلان  = >** ${member}`)     
+    channel.send(`**Invited by = >** ${Invite.inviter}`)     
+             
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
