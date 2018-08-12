1d59b30154dc9b7440a2c7f73d76d115c3bd62bb
hamossss <40631448+hamossss@users.noreply.github.com>
2018-08-12 11:29:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -262,5 +262,22 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
     
    }
    }); 
+client.on('ready', function(){    
+    var ms = 40000 ;    
+    var setGame = ['خاص بي حمو','$help'];    
+    var i = -1;    
+    var j = 0;    
+    setInterval(function (){    
+        if( i == -1 ){    
+j = 1;    
+       }    
+        if( i == (setGame.length)-1 ){    
+            j = -1;    
+      }    
+       i = i+j;    
+        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
+}, ms);    
+    
+});
    
 client.login(process.env.BOT_TOKEN);
